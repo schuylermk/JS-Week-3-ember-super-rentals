3a106b440aa0c99e6497bc2572c12d742a0af3ee
schuylermk <schuyler.m.k@gmail.com>
2016-03-27 17:45:46 -0700

    Adds rental data to firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'superior-rentals',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://superior-rentals.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
